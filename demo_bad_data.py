2bec7876a829a78ef1b3ab5c1f38703cecf37fe7
mirzezade00 <ay.mirzeyeva00@gmail.com>
2026-09-07 00:24:14 +0400

    updated

--- demo_bad_data.py
+++ demo_bad_data.py
@@ -1,41 +1,27 @@
-from ingestion import run_ingestion
 import pandas as pd
-import logging
-import numpy as np
 from validation import current_schema
-from config import CONFIG
-from loader import (
-    get_high_water_mark,
-    load_current_snapshot,
-    load_historical_data,
-    load_quarantine_data
-)
-import pandera.pandas as pa
 from pandera.errors import SchemaErrors
-
 
 bad_data = pd.DataFrame(
     {
-        "coin_id": ['bitcoin'],
-        "price": [-50],
-        'symbol':['btb'],
-        'date' : ['today'],
-        'volume': [200000],
-        'market_cap':[ 100000],
-        'name' : ['Bitcoin']
+        "coin_id": ["bitcoin"],
+        "price": [-50.0],
+        "symbol": ["btc"],
+        "date": [pd.Timestamp("2026-09-01").date()],
+        "volume": [200000.0],
+        "market_cap": [100000.0],
+        "name": ["Bitcoin"],
     }
 )
-print('Bad data:')
+
+print("Bad data:")
 print(bad_data)
 
 try:
-    current_schema.validate(bad_data,
-                            lazy= True)
+    current_schema.validate(bad_data, lazy=True)
+    print("Data validdir!")
     
-    print('Data validdir!')
-    
-except pa.errors.SchemaError as exc:
-    print('Data Rejected!')
-    print('Data invaliddir!')
-    print(exc)
+except SchemaErrors as exc:
+    print("Data Rejected!")
+    print("Data invaliddir!")
     print(exc.failure_cases)
